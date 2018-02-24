6f42e0396e239175add510d1cec6e1d05f34cabf
kilchem182 <32198896+kilchem182@users.noreply.github.com>
2018-02-24 17:14:33 -0500

    Added a Procfile

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var player_names = [];
+
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
 });
@@ -12,11 +14,15 @@ io.on('connection', function(socket){
     socket.on('chat message',function(msg){
       io.emit('chat message', msg);
       console.log('message: ' + msg);
-    })
+    });
+    socket.on('ready',function(name){
+	player_names.append(name);
+	console.log(player_names);
+    });
     socket.on('disconnect', function(){
       io.emit('chat message', 'a user has disconnected')
       console.log('user disconnected');
-    })
+    });
 });
 
 http.listen((process.env.PORT||3000), function(){
