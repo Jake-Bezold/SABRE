5bdab6cd166120cb4967001af0e7aca06bc928b5
Jake Bezold <crackerjake@optonline.net>
2018-02-25 09:22:31 -0500

    i really need to sleep

--- index.js
+++ index.js
@@ -102,9 +102,8 @@ request.origin + ' rejected.');
        if (message.type === 'utf8') {
            console.log('Received Message: ' + message.utf8Data);
            console.log('Message Type: ' + typeof(message));
-           connection.sendUTF(message.utf8Data);
            var data = JSON.parse(String(message));
-           player_names[playerNum] = data['userName'];
+           player_names[playerNum] = data["userName"];
            console.log("userName: " + player_names[playerNum])
            gameStart();
        }
