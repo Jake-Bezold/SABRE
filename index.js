f029d1be438d1c29e0236f9d113736149aa1a4e9
Jake Bezold <crackerjake@optonline.net>
2018-02-25 05:11:03 -0500

    working game mechanics

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ players['player1'] = {};
 players['player2'] = {};
 
 objects.push(['player 1', 0.0, 0.0, 20, .5, 1, 2, 2.0, 1]);
-var objectsJSON = JSON.stringify(objects);
-console.log(objectsJSON)
+objects.push(['player 2', 4.0, 4.0, 20, .5, 1, 2, 2.0, 2]);
+commands.push(["move", 0, 10, [0,0], [4,4], true]);
+commands.push([null]);
+for (testiter = 0; testiter <= 20; testiter++){
+  var objectsJSON = JSON.stringify(objects);
+  console.log(testiter + ": " + objectsJSON);
+  console.log("commands: " + commands);
+  tick();
+}
+
 
 
 
@@ -90,9 +98,9 @@ message.binaryData.length + ' bytes');
    }
 
 //ticks contiously and executes commands when needed
-   function tick(commands) {
+   function tick() {
      for (i = 0 ; i < commands.length; i++){
-       if (commands[i] != nil) {
+       if (commands[i] != null) {
          execute(commands[i])
        }
      }
@@ -100,59 +108,101 @@ message.binaryData.length + ' bytes');
 
 //calls whatever function is needed based on command type
    function execute(cmd) {
-     if (cmd[0] == "move") {
+     if (cmd[0] == null){
+       return;
+     } else if (cmd[0] == "move") {
        move(cmd)
-     } else {
+     } else if (cmd[0] == "battle"){
        battle(cmd)
+     } else if (cmd[0] == "idle"){
+     } else {
+       console.log("invalid command: " + cmd[0]);
      }
    }
 
 //move function moves units the units to the "to" and if encounters enemy units
 //calls the battle function
+
    function move(cmd){
-     if (cmd[5] == true){
-       var cur_obj = objects[cmd[1]];
-       objects.append(cur_obj);
-       objects[-1][3] = cmd[2];
-       cur_obj[3] -= cmd[2];
+     var cur_obj;
+     cur_obj = objects[cmd[1]];
+     console.log("cmd[2]: " + cmd[2] + ", objects[cmd[1]][2]: " + objects[cmd[1]][3] + ", first?: " + cmd[5]);
+     if (cmd[5] == true && cmd[2] == objects[cmd[1]][3]){
+       commands[cmd[1]][5] = false;
      }
+     else if (cmd[5] == true){
+       //console.log(commands);
+
+       objects.push(cur_obj.slice());
+
+       objects[objects.length-1][3] = cmd[2];
+
+       objects[cmd[1]][3] -= cmd[2];
+
+       cur_obj = objects[objects.length-1];
+
+       commands.push(commands[cmd[1]]);
+       //console.log(commands);
+
+       commands[commands.length-1][5] = false;
+
+       commands[cmd[1]] = [null];
+
+       commands[commands.length-1][1] = commands.length-1;
+
+
+       //console.log(commands);
+     }
+
+     console.log("Moving obj id: " + cur_obj[0]);
+     var xi = cur_obj[1];
+     var yi = cur_obj[2];
+     var xf = cmd[4][0];
+     var yf = cmd[4][1];
+     var ms = cur_obj[4];
+     var dist = 1.0
      if (xi <= xf) {
        if ((xi + ms) > xf) {
-         xi = xf;
+         cur_obj[1] = xf;
        } else {
-         xi += ms;
+         cur_obj[1] += ms;
        }
      }
      if (xi >= xf) {
        if ((xi - ms) < xf) {
-         xi = xf;
+         cur_obj[1] = xf;
        } else {
-         xi -= ms;
+         cur_obj[1] -= ms;
        }
      }
      if (yi <= yf) {
        if ((yi + ms) > yf) {
-         yi = yf;
+         cur_obj[2] = yf;
        } else {
-         yi += ms;
+         cur_obj[2] += ms;
        }
      }
      if (yi >= yf) {
        if ((yi - ms) < yf) {
-         yi = yf;
+         cur_obj[2] = yf;
        } else {
-         yi -= ms;
+         cur_obj[2] -= ms;
        }
+     }
+     var xi = cur_obj[1];
+     var yi = cur_obj[2];
+     if((objects[objects.length-1][1] == xf) && (objects[objects.length-1][2] == yf)) {
+       commands[cmd[1]] = null;
      }
      for(i=0; i < objects.length; i++) {
-       if((objects[i][1] - xi < dist) && (objects[i][2] - yi < dist)) {
-         if(objects[i][8] != curr_obj[8]) {
-          commands[cmd[1]] = ["battle", cmd[1], objects.indexOf(objects[i][1])];
+       if((objects[i][1] - xi <= dist) && (objects[i][2] - yi <= dist)) {
+         if(objects[i][8] != cur_obj[8] && i != objects.indexOf(cur_obj)) {
+           console.log("Starting battle with objects: " + objects.indexOf(cur_obj) + ", " + i);
+           commands[objects.indexOf(cur_obj)] = ["battle", objects.indexOf(cur_obj), i];
+           commands[i] = ["idle"];
+
          }
        }
-     }
-     if((xi == xf) && (yi == yf)) {
-       cmd = nil;
      }
    }
 
@@ -162,13 +212,17 @@ message.binaryData.length + ' bytes');
    function battle(cmd){
      objects[cmd[2]][3] -= objects[cmd[1]][6];
      objects[cmd[1]][3] -= objects[cmd[2]][6];
-     if (objects[2][3] <= 0){
-       commands.splice(2 , 1);
-       objects.splice(2, 1);
+     if (objects[cmd[2]][3] <= 0 || objects[cmd[1]][3] <= 0){
+       commands[cmd[1]] = [null];
+       commands[cmd[2]] = [null];
      }
-     else if (objects[1][3] <= 0){
-       commands.splice(1, 3);
-       objects.splice(1, 3);
+     if (objects[cmd[2]][3] <= 0){
+       commands.splice(cmd[2] , 1);
+       objects.splice(cmd[2], 1);
+     }
+     else if (objects[cmd[1]][3] <= 0){
+       commands.splice(cmd[1], 1);
+       objects.splice(cmd[1], 1);
      }
    }
 
