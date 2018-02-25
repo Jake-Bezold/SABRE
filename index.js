f7485146375efce343557af4e1e9b80ac804ec3b
Jake Bezold <crackerjake@optonline.net>
2018-02-24 19:56:28 -0500

    added name and ID functionallity

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var player_names = [];
+var players = {};
+players['player1'] = {};
+players['player2'] = {};
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
@@ -10,6 +12,17 @@ res.sendFile(__dirname + '/index.html');
 
 
 io.on('connection', function(socket){
+  //gets the users address from when connected
+  if (players['player1'] === {}){
+    players['player1']['id'] = socket.handshake.query.name
+    console.log(players['player1']['id'])
+  }
+    else if (players['player2'] === {}){
+    players['player2']['id'] = socket.handshake.query.name
+  }
+    else {
+      io.emit('chat message', 'error: two players have already entered')
+    }
     //sends "user connected" to page
     io.emit('chat message', 'a user has connected')
     //sends "user connected" to console
@@ -22,7 +35,17 @@ io.on('connection', function(socket){
       console.log('message: ' + msg);
     });
     socket.on('ready',function(name){
-	console.log(name);
+    	console.log(name);
+      if (players['player1'] === {}){
+        players['player1']['name'] = name.handshake.query.names
+      }
+      else if (players['player2']['name'] === {}){
+        players['player2']['name'] = name.handshake.query.names
+      }
+      else{
+        io.emit('chat message', 'error: two players have already entered')
+      }
+
 	//player_names.append(name);
 	//console.log(player_names);
     });
